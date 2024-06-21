ba2c8b927f36c3f5cb549a7d2e90f1c945150571
kwongngu <kwongngu@uwaterloo.ca>
2024-06-21 12:10:15 -0400

    Add files via upload

--- main.bat
+++ main.bat
@@ -1,8 +1,10 @@
 @echo off
 
+set "install=n"
+
 set /p install= Install necessary modules? (y/[n])
 if "%install%"=="y" (
-	call %~dp0\scripts\install.bat
+	call "%~dp0\scripts\install.bat"
 )
 
 python.exe "%~dp0\main.py"
