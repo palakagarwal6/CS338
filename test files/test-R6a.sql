f622e02c607a2e57ad1ee6b2c326bcfbfeec71a0
kwongngu <kwongngu@uwaterloo.ca>
2024-06-21 12:39:55 -0400

    Delete test files directory

--- test files/test-R6a.sql
+++
@@ -1,6 +0,0 @@
-USE netflix;
-
-SELECT title
-FROM movie 
-WHERE title LIKE '%Star Wars%'
-INTO OUTFILE "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\test-R6a.out";
--- test files/test-R7.sql
+++
@@ -1,6 +0,0 @@
-USE netflix;
-
-SELECT title, CONCAT("id: ", movie_id), vote_average, status, runtime, overview
-FROM movie 
-WHERE movie_id = 11
-INTO OUTFILE "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\test-R7.out";
