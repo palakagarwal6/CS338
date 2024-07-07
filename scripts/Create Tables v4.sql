60f86e0afcd9736aef120fb049ce291787e680b5
kwongngu <kwongngu@uwaterloo.ca>
2024-07-06 20:47:29 -0400

    Add files via upload

---
+++ scripts/Create Tables v4.sql
@@ -0,0 +1,144 @@
+USE netflix;
+
+CREATE TABLE Movie (
+	movie_id INT NOT NULL,
+    title Varchar(100) NOT NULL DEFAULT 'No Title',
+    overview Varchar(8000) DEFAULT 'No Overview', # for large paragraphs
+    `status` Varchar(20) NOT NULL,
+    release_date Varchar(50), # will change to date format later
+    adult Varchar(5) DEFAULT 'False', # will be booleans
+    video Varchar(5) DEFAULT 'False', # will be booleans
+    runtime DECIMAL(5,1) DEFAULT 0,
+    vote_average DECIMAL(2,1) DEFAULT 0.0,
+    vote_count INT DEFAULT 0,
+    PRIMARY KEY(movie_id)
+);
+
+ALTER TABLE Movie ADD CONSTRAINT movie_id CHECK (movie_id >= 0);
+ALTER TABLE Movie ADD CONSTRAINT status CHECK (status IN ('Released','Rumored','Post Production','In Production','Planned'));
+UPDATE Movie SET release_date = STR_TO_DATE(release_date, '%Y-%m-%d');
+ALTER TABLE Movie ADD CONSTRAINT adult CHECK (adult IN ('True','False'));
+ALTER TABLE Movie ADD CONSTRAINT runtime CHECK (runtime >= 0);
+ALTER TABLE Movie ADD CONSTRAINT vote_average CHECK (vote_average >= 0);
+ALTER TABLE Movie ADD CONSTRAINT vote_count CHECK (vote_count >= 0);
+
+CREATE TABLE Credit (
+    person_id INT NOT NULL,
+	name Varchar(100),
+	PRIMARY KEY(person_id)
+);
+
+ALTER TABLE Credit ADD CONSTRAINT person_id CHECK (person_id >= 0);
+
+CREATE TABLE Cast (
+	cast_id INT NOT NULL,
+    movie_id INT NOT NULL,
+    person_id INT NOT NULL,
+    `Character` Varchar(100),
+    UNIQUE (movie_id, cast_id),
+    FOREIGN KEY (person_id) REFERENCES Credit (person_id)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+	FOREIGN KEY (movie_id) REFERENCES movie (movie_id)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+	PRIMARY KEY(cast_id, person_id)
+);
+
+CREATE TABLE Crew (
+	person_id INT NOT NULL,
+    FOREIGN KEY (person_id) REFERENCES Credit (person_id)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+	PRIMARY KEY(person_id)
+);
+
+CREATE TABLE Department(
+	Dept_Name Varchar(50) UNIQUE,
+    PRIMARY KEY(Dept_Name)
+);
+
+CREATE TABLE Job(
+	Job_Name Varchar(50) UNIQUE,
+    PRIMARY KEY(Job_Name)
+);
+
+CREATE TABLE Performs (
+	Job_Name Varchar(50),
+	person_id INT NOT NULL,
+    movie_id INT NOT NULL,
+    FOREIGN KEY (Job_Name) REFERENCES Job (Job_Name)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (person_id) REFERENCES Crew (person_id)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+	FOREIGN KEY (movie_id) REFERENCES movie (movie_id)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+	PRIMARY KEY(Job_Name, person_id, movie_id)
+);
+
+CREATE TABLE Assigned_To (
+	Dept_Name Varchar(50),
+	person_id INT NOT NULL,
+    movie_id INT NOT NULL,
+    FOREIGN KEY (Dept_Name) REFERENCES Department (Dept_Name)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (person_id) REFERENCES Crew (person_id)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+	FOREIGN KEY (movie_id) REFERENCES movie (movie_id)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+	PRIMARY KEY(Dept_Name, person_id, movie_id)
+);
+
+CREATE TABLE Production (
+	production_id INT NOT NULL,
+	production_name Varchar(100),
+	PRIMARY KEY(production_id)
+);
+
+CREATE TABLE Genre (
+	genre_id INT NOT NULL,
+	genre_name Varchar(50),
+	PRIMARY KEY(genre_id)
+);
+
+CREATE TABLE Produced_By (
+	movie_id INT NOT NULL,
+    production_id INT NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES Movie (movie_id)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+	FOREIGN KEY (production_id) REFERENCES Production (production_id)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+	PRIMARY KEY(movie_id, production_id)
+);
+
+CREATE TABLE Classified_In (
+	movie_id INT NOT NULL,
+	genre_id INT NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES Movie (movie_id)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+	FOREIGN KEY (genre_id) REFERENCES Genre (genre_id)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+	PRIMARY KEY(movie_id, genre_id)
+);
+
+CREATE TABLE Comprises_Of (
+	movie_id INT NOT NULL,
+	person_id INT NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES Movie (movie_id)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+	FOREIGN KEY (person_id) REFERENCES Credit (person_id)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+	PRIMARY KEY(movie_id, person_id)
+);
---
+++ scripts/load_data v3.sql
@@ -0,0 +1,93 @@
+USE netflix;
+SET GLOBAL local_infile=1;
+
+LOAD DATA LOCAL INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.4\\Uploads\\movie.csv'
+INTO TABLE movie
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.4\\Uploads\\genre.csv'
+INTO TABLE genre
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.4\\Uploads\\credit.csv'
+INTO TABLE credit
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.4\\Uploads\\cast.csv'
+INTO TABLE cast
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.4\\Uploads\\crew.csv'
+INTO TABLE crew
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.4\\Uploads\\Department.csv'
+INTO TABLE Department
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.4\\Uploads\\Job.csv'
+INTO TABLE Job
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.4\\Uploads\\Performs.csv'
+INTO TABLE Performs
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.4\\Uploads\\Assigned_To.csv'
+INTO TABLE Assigned_To
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.4\\Uploads\\production.csv'
+INTO TABLE production
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.4\\Uploads\\produced_by.csv'
+INTO TABLE produced_by
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.4\\Uploads\\comprises_of.csv'
+INTO TABLE comprises_of
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.4\\Uploads\\classified_in.csv'
+INTO TABLE classified_in
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
