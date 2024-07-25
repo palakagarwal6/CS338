3dffe69ee833ea4f45b1159842c2c9769fe0502b
Palak Agarwal <palakagarwal662@gmail.com>
2024-07-24 23:21:55 -0400

    db final

--- GUI/db.py
+++ GUI/db.py
@@ -42,11 +42,15 @@ def use_database(cnx: MySQLConnection, db_name: str) -> None:
             cursor.execute(f"USE {db_name}")
 
 
-def create_movies_table(cnx: MySQLConnection) -> bool:
+def create_movies_table(cnx: MySQLConnection, feature: str = "Statistics") -> bool:
+    if feature == "Statistics":
+        name = "Movie"
+    else:
+        name = "movies"
     if cnx and cnx.is_connected():
         with cnx.cursor() as cursor:
-            query = """
-            CREATE TABLE IF NOT EXISTS Movie(
+            query = f"""
+            CREATE TABLE IF NOT EXISTS {name}(
                 movie_id INT PRIMARY KEY,
                 title VARCHAR(255),
                 overview TEXT,
@@ -62,9 +66,23 @@ def create_movies_table(cnx: MySQLConnection) -> bool:
             cursor.execute(query)
 
 
-def populate_movies_table(cnx: MySQLConnection, csv_file_path: str) -> None:
+def populate_movies_table(cnx: MySQLConnection, csv_file_path: str, update_progress, feature: str = "Statistics") -> None:
+    if feature == "Statistics":
+        name = "Movie"
+    else:
+        name = "movies"
+
     df = pd.read_csv(csv_file_path)
     df = df.drop_duplicates(subset='movie_id', keep='first')
+
+    df['movie_id'] = pd.to_numeric(df['movie_id'], errors='coerce')
+    df['adult'] = df['adult'].astype(bool, errors='ignore')
+    df['runtime'] = pd.to_numeric(df['runtime'], errors='coerce')
+    df['vote_average'] = pd.to_numeric(df['vote_average'], errors='coerce')
+    df['vote_count'] = pd.to_numeric(df['vote_count'], errors='coerce')
+
+    df.dropna(subset=['movie_id', 'runtime',
+              'vote_average', 'vote_count'], inplace=True)
 
     df = df.astype({
         'movie_id': int,
@@ -77,9 +95,10 @@ def populate_movies_table(cnx: MySQLConnection, csv_file_path: str) -> None:
         'vote_count': int
     })
 
-    for _, row in df.iterrows():
-        query = """
-        INSERT INTO Movie(movie_id, title, overview, status, release_date, adult, video, runtime, vote_average, vote_count)
+    total_rows = len(df)
+    for i, (_, row) in enumerate(df.iterrows()):
+        query = f"""
+        INSERT IGNORE INTO {name}(movie_id, title, overview, status, release_date, adult, video, runtime, vote_average, vote_count)
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
 
@@ -89,11 +108,13 @@ def populate_movies_table(cnx: MySQLConnection, csv_file_path: str) -> None:
             with cnx.cursor() as cursor:
                 cursor.execute(query, values)
 
+        update_progress(i + 1, total_rows)
+
 
 def load_all_movies_from_db(cnx: MySQLConnection) -> pd.DataFrame:
     if cnx and cnx.is_connected():
         with cnx.cursor() as cursor:
-            query = "SELECT * FROM Movie"
+            query = "SELECT * FROM movies"
             df = pd.read_sql(query, cnx)
             return df
 
@@ -102,7 +123,7 @@ def update_movie_details_in_db(cnx: MySQLConnection, row: pd.Series) -> None:
     if cnx and cnx.is_connected():
         with cnx.cursor() as cursor:
             query = """
-            UPDATE Movie
+            UPDATE movies
             SET 
                 title = %s,
                 overview = %s,
@@ -136,8 +157,8 @@ def delete_movie_from_db(cnx: MySQLConnection, movie_id: int) -> None:
     if cnx and cnx.is_connected():
         with cnx.cursor() as cursor:
             movie_id = int(movie_id)
-            cursor.eXxecute(
-                "DELETE FROM Movie WHERE movie_id = %s", [movie_id])
+            cursor.execute(
+                "DELETE FROM movies WHERE movie_id = %s", [movie_id])
 
 
 def delete_table(cnx: MySQLConnection, table_name: str) -> None:
@@ -159,6 +180,7 @@ def get_crew_statistics(cnx: MySQLConnection) -> pd.DataFrame:
         JOIN Credit cr ON c.person_id = cr.person_id
         GROUP BY cr.name, c.person_id, j.job_name
         ORDER BY avg_rating DESC
+        LIMIT 100
         """
         df = pd.read_sql(query, cnx)
         return df
@@ -182,7 +204,7 @@ def populate_genre_table(cnx: MySQLConnection, csv_file_path: str) -> None:
 
         with cnx.cursor() as cursor:
             for _, row in data.iterrows():
-                query = f"INSERT INTO Genre (genre_id, name) VALUES (%s, %s)"
+                query = f"INSERT IGNORE INTO Genre (genre_id, name) VALUES (%s, %s)"
                 cursor.execute(query, tuple(row))
 
         cnx.commit()
@@ -196,7 +218,7 @@ def populate_classified_in_table(cnx: MySQLConnection, csv_file_path: str) -> No
 
         with cnx.cursor() as cursor:
             for _, row in data.iterrows():
-                query = f"INSERT INTO Classified_In (movie_id, genre_id) VALUES (%s, %s)"
+                query = f"INSERT IGNORE INTO Classified_In (movie_id, genre_id) VALUES (%s, %s)"
                 cursor.execute(query, tuple(row))
 
         cnx.commit()
@@ -266,8 +288,8 @@ def create_classified_in_table(cnx: MySQLConnection) -> None:
                 movie_id INT,
                 genre_id INT,
                 PRIMARY KEY (movie_id, genre_id),
-                FOREIGN KEY (movie_id) REFERENCES Movie(movie_id),
-                FOREIGN KEY (genre_id) REFERENCES Genre(genre_id)
+                FOREIGN KEY (movie_id) REFERENCES Movie(movie_id) ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (genre_id) REFERENCES Genre(genre_id) 
             );
             """
             cursor.execute(query)
@@ -279,7 +301,7 @@ def populate_classified_in_table(cnx: MySQLConnection, csv_file_path: str) -> No
 
         with cnx.cursor() as cursor:
             for _, row in data.iterrows():
-                query = f"INSERT INTO Classified_In (movie_id, genre_id) VALUES (%s, %s)"
+                query = f"INSERT IGNORE INTO Classified_In (movie_id, genre_id) VALUES (%s, %s)"
                 cursor.execute(query, tuple(row))
 
         cnx.commit()
@@ -309,7 +331,7 @@ def populate_production_table(cnx: MySQLConnection, csv_file_path: str) -> None:
 
         with cnx.cursor() as cursor:
             for _, row in data.iterrows():
-                query = f"INSERT INTO Production VALUES (%s, %s)"
+                query = f"INSERT IGNORE INTO Production VALUES (%s, %s)"
                 cursor.execute(query, tuple(row))
 
         cnx.commit()
@@ -328,7 +350,7 @@ def create_produced_by_table(cnx: MySQLConnection) -> None:
                 movie_id INT,
                 production_id INT,
                 PRIMARY KEY (movie_id, production_id),
-                FOREIGN KEY (movie_id) REFERENCES Movie(movie_id),
+                FOREIGN KEY (movie_id) REFERENCES Movie(movie_id) ON DELETE CASCADE ON UPDATE CASCADE,
                 FOREIGN KEY (production_id) REFERENCES Production(production_id)
             );
             """
@@ -341,7 +363,7 @@ def populate_produced_by_table(cnx: MySQLConnection, csv_file_path: str) -> None
 
         with cnx.cursor() as cursor:
             for _, row in data.iterrows():
-                query = f"INSERT INTO Produced_By VALUES (%s, %s)"
+                query = f"INSERT IGNORE INTO Produced_By VALUES (%s, %s)"
                 cursor.execute(query, tuple(row))
 
         cnx.commit()
@@ -370,8 +392,12 @@ def populate_crew_table(cnx: MySQLConnection, csv_file_path: str) -> None:
 
         with cnx.cursor() as cursor:
             for _, row in data.iterrows():
-                query = f"INSERT INTO Crew VALUES (%s)"
-                cursor.execute(query, tuple(row))
+                try:
+                    query = f"INSERT IGNORE INTO Crew VALUES (%s)"
+                    cursor.execute(query, tuple(row))
+                except Exception as e:
+                    print("Error processing", row)
+                    print(e)
 
         cnx.commit()
 
@@ -391,7 +417,7 @@ def create_performs_table(cnx: MySQLConnection) -> None:
                 movie_id INT,
                 PRIMARY KEY (person_id, movie_id),
                 FOREIGN KEY (person_id) REFERENCES Credit(person_id),
-                FOREIGN KEY (movie_id) REFERENCES Movie(movie_id),
+                FOREIGN KEY (movie_id) REFERENCES Movie(movie_id) ON DELETE CASCADE ON UPDATE CASCADE,
                 FOREIGN KEY (job_name) REFERENCES Job(job_name)
             );
             """
@@ -432,7 +458,7 @@ def populate_job_table(cnx: MySQLConnection, csv_file_path: str) -> None:
 
         with cnx.cursor() as cursor:
             for _, row in data.iterrows():
-                query = f"INSERT INTO Job VALUES (%s)"
+                query = f"INSERT IGNORE INTO Job VALUES (%s)"
                 cursor.execute(query, tuple(row))
 
         cnx.commit()
@@ -462,7 +488,7 @@ def populate_credit_table(cnx: MySQLConnection, csv_file_path: str) -> None:
 
         with cnx.cursor() as cursor:
             for _, row in data.iterrows():
-                query = f"INSERT INTO Credit VALUES (%s, %s)"
+                query = f"INSERT IGNORE INTO Credit VALUES (%s, %s)"
                 cursor.execute(query, tuple(row))
 
         cnx.commit()
