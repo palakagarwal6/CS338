dbb520acaade78aca4e8e177327d353362726902
kwongngu <kwongngu@uwaterloo.ca>
2024-07-30 16:01:08 -0400

    Add files via upload
    added GUI option

--- main.bat
+++ main.bat
@@ -2,9 +2,18 @@
 
 set "install=n"
 
-set /p install= Install necessary modules? (y/[n])
+set /p install= Install necessary modules (required for program to run)? (y/[n])
 if "%install%"=="y" (
 	call "%~dp0\scripts\install.bat"
 )
 
-py "%~dp0\main.py"
+echo 1: CLI
+echo 2: GUI
+
+set /p ui= which UI would you like to use? ([1]/2)
+if "%ui%"=="2" (
+	streamlit run "%~dp0\GUI\final_version_GUI.py"
+) else (
+	py "%~dp0\main.py"
+)
+
